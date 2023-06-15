9827094773cdfa9b99a9273fafcaca6ae136d9d5
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-06-15 21:08:01 +0200

    super_commit

--- patcher/install_patch.sh
+++ patcher/install_patch.sh
@@ -1,4 +1,4 @@
-mv bk439964.patch /
+cp patcher/bk439964.patch /
 cd /
 patch -t -p1 < bk439964.patch
 cd /usr/src
--- patcher/make_patch.sh
+++ patcher/make_patch.sh
@@ -1,3 +1,2 @@
 cp patcher/striped.patch patcher/bk439964.patch
 diff -rupNEZbB old_mount/usr fake_mount/usr >> patcher/bk439964.patch
-mv patcher/bk439964.patch .
