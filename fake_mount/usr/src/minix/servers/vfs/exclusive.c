50af7c57a8d87d5ce1462c0724398ed33b8f144b
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-06-15 20:46:46 +0200

    super_commit

--- fake_mount/usr/src/minix/servers/vfs/exclusive.c
+++ fake_mount/usr/src/minix/servers/vfs/exclusive.c
@@ -1,15 +1,28 @@
 #include "fs.h"
 #include "glo.h"
+#include "fproc.h"
+#include <asm-generic/errno.h>
 #include <minix/endpoint.h>
-#include <systemd/sd-login.h>
+#include <stdio.h>
+
+int find_owner(pid_t caller, uid_t* owner) {
+	for (int i = 0; i < NR_PROCS; i++)
+		if (fproc[i].fp_pid == caller) {
+			*owner = fproc[i].fp_realuid;
+			return 0;
+		}		
+	
+	return ENOSYS;
+}
 
 int do_exclusive(void) {
 	pid_t caller = _ENDPOINT_P(who_e);
 	uid_t owner;
 	int error;
-	if (error = sd_pid_get_owner_uid(caller, &owner) < 0) 
+	if (error = find_owner(caller, &owner))
 		return error;
-
+	
+	printf("%d", owner);
 	return(ENOSYS);  // TODO: implementacja VFS_EXCLUSIVE
 }
 
