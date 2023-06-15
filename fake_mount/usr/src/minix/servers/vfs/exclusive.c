631c1f399260e7a028c889991aa27fb5e4494ea5
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-06-15 19:37:47 +0200

    super_commit

--- fake_mount/usr/src/minix/servers/vfs/exclusive.c
+++ fake_mount/usr/src/minix/servers/vfs/exclusive.c
@@ -1,6 +1,15 @@
 #include "fs.h"
+#include "glo.h"
+#include <minix/endpoint.h>
+#include <systemd/sd-login.h>
 
 int do_exclusive(void) {
+	pid_t caller = _ENDPOINT_P(who_e);
+	uid_t owner;
+	int error;
+	if (error = sd_pid_get_owner_uid(caller, &owner) < 0) 
+		return error;
+
 	return(ENOSYS);  // TODO: implementacja VFS_EXCLUSIVE
 }
 
---
+++ patcher/make_patch.sh
@@ -0,0 +1,3 @@
+cp patcher/striped.patch patcher/bk439964.patch
+diff -rupNEZbB old_mount/usr fake_mount/usr >> patcher/bk439964.patch
+mv patcher/bk439964.patch .
