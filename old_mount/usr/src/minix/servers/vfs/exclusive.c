fe4fa01531f063889d44ae9ab333ef0d1a405c46
Bartek Kucypera <bartek.kucypera@gazeta.pl>
2023-06-15 20:03:34 +0200

    super_commit

--- old_mount/usr/src/minix/servers/vfs/exclusive.c
+++
@@ -1,9 +0,0 @@
-#include "fs.h"
-
-int do_exclusive(void) {
-	return(ENOSYS);  // TODO: implementacja VFS_EXCLUSIVE
-}
-
-int do_fexclusive(void) {
-	return(ENOSYS);  // TODO: implementacja VFS_FEXCLUSIVE
-}
